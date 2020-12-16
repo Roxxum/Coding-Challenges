60ff5e651a7d1b99e5230b6ffc09e61fcae64f51
Adam Pavey <adam@roxxum.tech>
2020-12-16 11:48:48 +1000

    Added a Template

---
+++ Template.py
@@ -0,0 +1,16 @@
+"""
+Coding Challange description goes here
+"""
+
+# define an input for testing purposes
+input = 'some value'
+
+#actual code to submit
+def solution:
+    if something:
+        do something
+    else:
+        do something else
+
+#use print statement to check if it works
+print(solution)
